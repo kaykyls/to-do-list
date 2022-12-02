4d0b44b0d977fc7071f7b700a992e2616b7c0a5a
kaykyls <20211bsifsa0025@ifba.edu.br>
2022-12-02 20:31:33 -0300

    start working on editing

--- src/components/todoItem.jsx
+++ src/components/todoItem.jsx
@@ -1,20 +1,46 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const TodoItem = (props) => {
+    const [editTodo, setEditTodo] = useState(false)
+    const [editTodoText, setEditTodoText] = useState(props.todo)
+
+    useEffect(() => {
+        if(editTodo) {
+            const inputEdit = document.querySelector(`li:nth-child(${props.id + 1}) .todo-text-edit`)
+            inputEdit.focus()
+        }   
+    }, [editTodo])
+
     const isChecked = (e) => {
         if(e.target.checked)
         document.querySelector(`li:nth-child(${props.id + 1}) .todo-text`).style.textDecoration = "line-through"
         else
         document.querySelector(`li:nth-child(${props.id + 1}) .todo-text`).style.textDecoration = "none"
     }
+    
+    const handleEdit = () => {
+        editTodo ? setEditTodo(false) : setEditTodo(true)
+    }
 
-    const handleEdit = (e) => {
-        const text = document.querySelector(`li:nth-child(${props.id + 1}) .todo-text`)
-        text.focus()
+    let todoText
+    let buttons
 
-        // console.log(props.id)
-        // console.log(li)
-        // props.updateTodo(props.id)
+    if(editTodo) {
+        todoText = <input className="todo-text-edit" type="text" defaultValue={props.todo}></input>
+
+        buttons = <div className="buttons">
+                        <button onClick={handleEdit}><span className="material-symbols-outlined">done</span></button>
+                        <button onClick={handleEdit}><span className="material-symbols-outlined">close</span></button>
+                </div>
+    } else {
+        todoText = <span className="todo-text">{props.todo}</span>;
+
+        buttons = <div className="buttons">
+                    <button onClick={handleEdit}><span className="material-symbols-outlined">edit</span></button>
+                    <button onClick={() => props.deleteTodo(props.id)}><span className="material-symbols-outlined">delete</span></button>
+                </div>
     }
 
     return (
@@ -22,12 +48,9 @@ const TodoItem = (props) => {
             <div className="todo-item-container">
                 <div className="text">
                     <input onChange={isChecked} type="checkbox"/>
-                    <input className="todo-text" type="text" defaultValue={props.todo}></input>
+                    {todoText}
                 </div>
-                <div className="buttons">
-                    <button onClick={handleEdit}><span className="material-symbols-outlined">edit</span></button>
-                    <button onClick={() => props.deleteTodo(props.id)}><span className="material-symbols-outlined">delete</span></button>
-                </div>
+                {buttons}
             </div>
         </li>
     )
